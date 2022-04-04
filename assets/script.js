b719319ca87be2ae542e1da7a7998d789e9d49e5
masomj <masonjayjones@gmail.com>
2022-04-04 21:09:16 +0100

    Added temp random word selector. Began code for enter button

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,72 @@
 let selectedIndex = 0;
 let guesses = 1;
-let guessedWord ="";
+let guessedWord =""; //the user input word
 let guessedLetters =[];
+let word = ""; //The word to guess
 
+const wordList = ['aahed', 'stays', 'hello', 'words', 'sword', 'clone', 'those', 'slave', 'stool', 'bytes', 'silly', 'spore', 'shore', 'index', 'style', 'creed', 'trust', 'tryst', 'scare', 'share', 'where', 'splay'];
+
+//get random word
+function selectWord(wordList) { 
+    let i = Math.floor(Math.random() * wordList.length)
+    console.log[i]
+    return wordList[i].toUpperCase();
+}
+//to set up the game on start up
+function onDocLoad(){
+    word = selectWord(wordList);
+    console.log(word);
+}
+
+//Calls the relevant function
+function checkAnswer(){
+    if (guessedWord === word){
+        correctWord();
+    } else if (guessedWord != word){
+        incorrectWord();
+    }
+}
+
+function correctWord(){
+    console.log('You Won');
+    changeColor("correct")
+}
+
+function incorrectWord(){
+    console.log('You Lost');
+    changeColor("incorrect")
+}
+
+function changeColor(result){
+    let indexes = document.getElementById(`Guess${guesses}`).children;
+    if (result === 'correct'){
+        for (let i = 0; i < 5; i++){
+            indexes[i].style.backgroundColor ='green'; 
+        } 
+    }else if (result === 'incorrect') {
+        for (let i = 0; i < 5; i++){
+            if (guessedLetters[i] === word[i]){
+                indexes[i].style.backgroundColor = 'green';
+            } else if (word.includes(guessedLetters[i])){
+                indexes[i].style.backgroundColor = 'yellow';
+            }
+        }
+    }
+}
+
+
+//Adds the selected letter to the selected index and builds the guessed word
 function performAddLetter(guessNumber, letter) {
     let indexes = document.getElementById(guessNumber).children;
     indexes[selectedIndex].innerHTML = String(letter);
     guessedWord += letter;
     guessedLetters.push(letter);
-    console.log(guessedWord);
-    console.log(selectedIndex);
 };
 
+
+//calls the addLetter function on the correct guess row
 function addLetter(evnt) {
-    let letter = evnt.target.textContent;
+    let letter = evnt.target.textContent; 
     if (selectedIndex <=5 ){
         switch (guesses) {
             case 1:        
@@ -45,4 +98,10 @@ letters = document.getElementsByClassName("letter");
 for (let letter of letters){
     letter.addEventListener('click', addLetter)
 };
+//calls the game load function on dom load
+document.addEventListener('DOMContentLoaded', onDocLoad, false)
+
+//Loads enter function onto the enter button on Keyboard
+let enter = document.getElementById("enter");
+enter.addEventListener('click', checkAnswer);
 
